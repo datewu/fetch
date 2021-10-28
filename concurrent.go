d9e178cee3ffb37a42175ee042c931cf6b3485d5
datewu <hihahajun@gmail.com>
2021-10-28 10:29:59 +0800

    add concurrent fetch

---
+++ concurrent.go
@@ -0,0 +1,58 @@
+package fetch
+
+import (
+	"errors"
+	"sync"
+)
+
+// Fetcher use with closure
+type Fetcher interface {
+	Fetch() (interface{}, error)
+}
+
+// ConcurrentFirstOrErr concurrent fetch the fetcher list
+// return the first successful result or err is
+// all fetchers failed.
+func ConcurrentFirstOrErr(fs []Fetcher) (interface{}, error) {
+	if len(fs) == 0 {
+		return nil, errors.New("empty fetcher list")
+	}
+	var wg sync.WaitGroup
+	wg.Add(len(fs))
+	done := make(chan struct{})
+	resStream := make(chan interface{})
+	errStream := make(chan error)
+	for _, f := range fs {
+		go func(lookup Fetcher) {
+			defer wg.Done()
+			res, err := lookup.Fetch()
+			if err != nil {
+				select {
+				case <-done:
+				case errStream <- err:
+				}
+				return
+			}
+			select {
+			case <-done:
+			case resStream <- res:
+			}
+		}(f)
+	}
+	go func() {
+		wg.Wait()
+		close(resStream)
+		close(errStream)
+	}()
+	out := <-resStream
+	close(done)
+	if out != nil {
+		return out, nil
+	}
+	select {
+	case err := <-errStream:
+		return nil, err
+	default:
+		return nil, errors.New("no reust")
+	}
+}
